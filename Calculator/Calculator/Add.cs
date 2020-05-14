c492528ca5aeb7f73e1da5be17a10eb29639c473
Zbigniew Angelus <zangelus@gmail.com>
2020-05-14 07:02:58 -0400

    init

---
+++ Calculator/Calculator/Add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Add
+    {
+        public int Execute(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
